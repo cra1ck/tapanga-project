9053de74d5f2d07106b490497ddd8f006d2ad2bf
cra1ck <cra1ck21@gmail.com>
2022-07-23 00:11:53 +0300

    delete weong models

--- cork_board/board/models.py
+++ cork_board/board/models.py
@@ -4,13 +4,3 @@ from django.db import models
 
 # Create your models here.
 
-class Ball(models.Model):
-    """
-    Model save info about ball position and etc.
-    """
-    name = models.AutoField(max_length=200)
-    positionX = models.FloatField(null=True)
-    positionY = models.FloatField(null=True)
-
-    def __str__(self):
-        return self.name
